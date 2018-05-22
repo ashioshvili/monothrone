7f1f3a7691d33a31e74ebd67e4b588d8b90f2d43
Alexander Shioshvili <lex.shioshvili@gmail.com>
2018-05-23 00:02:05 +0400

    name

--- answer.py
+++ answer.py
@@ -2,9 +2,20 @@ import random
 def generate_answer(sender_name,messaging_text,entity,value):
 	response = None
 	greeting_list = ["გამარჯობა", "პრივეტ", "სალამი"]
+	abc = {'ა':('a'),'ბ':('b'),'გ':('g'),'დ':('d'),'ე':('e'),'ვ':('v'),'ზ':('z'),'თ':('t'),'ი':('i'),'კ':('k'),'ლ':('l'),'მ':('m'),'ნ':('n'),'ო':('o'),'პ':('p')
+			,'ჟ':('j','zh'),'რ':('r'),'ს':('s'),'ტ':('t'),'უ':('u'),'ფ':('f'),'ქ':('q','k'),'ღ':('g'),'ყ':('y','k'),'შ':('sh'),'ჩ':('ch'),'ც':('c','ts'),'ძ':('z','dz'),'წ':('ts','c','w'),'ჭ':('ch')
+			,'ხ':('x','kh'),'ჯ':('j'),'ჰ':('h')}
+	
+	name = ''
+	for char in range(len(sender_name)):
+		for key, val in abc.items():
+			for n in range(len(val)):
+				if char == val[0]
+					name += key
+			
 	if entity == 'greeting_keys':
 		greet_txt = random.choice(greeting_list)
-		response = "{greet} {name}!".format(greet=greet_txt,name=sender_name)
+		response = "{greet} {name}!".format(greet=greet_txt,name=name)
 	if response == None:
-		response = "ბოდიში {name}, '{answer}' ჯერ არ ვიცი რას ნიშნავს :)".format(name=sender_name,answer=messaging_text)
+		response = "ბოდიში {name}, '{answer}' ჯერ არ ვიცი რას ნიშნავს :)".format(name=name,answer=messaging_text)
 	return response
