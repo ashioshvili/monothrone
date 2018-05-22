53f2c78458fb6b8f88fb9ca4a7eb892b24c171c6
Alexander Shioshvili <lex.shioshvili@gmail.com>
2018-05-23 00:08:30 +0400

    g

--- answer.py
+++ answer.py
@@ -6,16 +6,16 @@ def generate_answer(sender_name,messaging_text,entity,value):
 			,'ჟ':('j','zh'),'რ':('r'),'ს':('s'),'ტ':('t'),'უ':('u'),'ფ':('f'),'ქ':('q','k'),'ღ':('g'),'ყ':('y','k'),'შ':('sh'),'ჩ':('ch'),'ც':('c','ts'),'ძ':('z','dz'),'წ':('ts','c','w'),'ჭ':('ch')
 			,'ხ':('x','kh'),'ჯ':('j'),'ჰ':('h')}
 	
-	name = ''
+	name_ge = ''
 	for char in range(len(sender_name)):
 		for key, val in abc.items():
 			for n in range(len(val)):
-				if char == val[0]
-					name += key
+				if char == val[0]:
+					name_ge += key
 			
 	if entity == 'greeting_keys':
 		greet_txt = random.choice(greeting_list)
-		response = "{greet} {name}!".format(greet=greet_txt,name=name)
+		response = "{greet} {name}!".format(greet=greet_txt,name=name_ge)
 	if response == None:
-		response = "ბოდიში {name}, '{answer}' ჯერ არ ვიცი რას ნიშნავს :)".format(name=name,answer=messaging_text)
+		response = "ბოდიში {name}, '{answer}' ჯერ არ ვიცი რას ნიშნავს :)".format(name=name_ge,answer=messaging_text)
 	return response
