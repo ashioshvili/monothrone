34d7aaef0953eb10639323259024bd32ce852a89
Alexander Shioshvili <lex.shioshvili@gmail.com>
2018-05-23 00:27:57 +0400

    ghjghj

--- answer.py
+++ answer.py
@@ -5,14 +5,39 @@ def generate_answer(sender_name,messaging_text,entity,value):
 	abc = {'ა':('a'),'ბ':('b'),'გ':('g'),'დ':('d'),'ე':('e'),'ვ':('v'),'ზ':('z'),'თ':('t'),'ი':('i'),'კ':('k'),'ლ':('l'),'მ':('m'),'ნ':('n'),'ო':('o'),'პ':('p')
 			,'ჟ':('j','zh'),'რ':('r'),'ს':('s'),'ტ':('t'),'უ':('u'),'ფ':('f'),'ქ':('q','k'),'ღ':('g'),'ყ':('y','k'),'შ':('sh'),'ჩ':('ch'),'ც':('c','ts'),'ძ':('z','dz'),'წ':('ts','c','w'),'ჭ':('ch')
 			,'ხ':('x','kh'),'ჯ':('j'),'ჰ':('h')}
-	
+	snm = sender_name.lower()
 	name_ge = ''
-	for char in range(len(sender_name)):
+	name_spell = ''
+	for n in range(len(snm)):
+		got_two_ch = 0
+		two_ch = snm[n] + snm[n+1]
 		for key, val in abc.items():
-			for n in range(len(val)):
-				if char == val[0]:
+			for nn in range(len(val)):
+				if two_ch == val[nn]:
+					got_two_ch = 1
 					name_ge += key
+		if got_two_ch == 0:
+			for key, val in abc.items():
+				if snm[n] == val[nn]:
+					name_ge += key
+		
+					
+		"""
+		if (sender_name[n] + sender_name[n+1]) == 'sh':
+			name_ge += 'შ'
 			
+		else:
+			if sender_name[0] == 'x':
+				name_ge += 'ხ'
+			if sender_name[0] == 'x':
+				name_ge += 'ხ'
+			if len(sender_name)-(n+1) != 0:
+				char = sender_name[n]
+				for key, val in abc.items():
+					for n in range(len(val)):
+						if char == val[0]:
+							name_ge += key
+		"""	
 	if entity == 'greeting_keys':
 		greet_txt = random.choice(greeting_list)
 		response = "{greet} {name}!".format(greet=greet_txt,name=name_ge)
