faac927d048add837de20f2b54cdc792a3afc82c
Alexander Shioshvili <32383409+ashioshvili@users.noreply.github.com>
2018-05-23 16:43:20 +0400

    sd

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def verify():
 		if not request.args.get("hub.verify_token") == "a1B2c3E4f5":
 			return "Verification token mismatch", 403
 		return request.args["hub.challenge"], 200
-	return "OK", 200
+	return "OK :: 200", 200
 	
 @app.route('/', methods=['POST'])
 def webhook():
